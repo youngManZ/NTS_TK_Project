66c8ddf26cc034e245b66e0d1d8f76dbda0bc5a2
nrh <nrh@nrhdeMacBook-Air.local>
2018-05-31 13:37:19 +0800

    log

---
+++ Desktop/NTS_TK_V2/NTS_TK_Provide_Manager/NTS_TK_Provide_Manager/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  NTS_TK_Provide_Manager
+//
+//  Created by nrh on 2018/5/31.
+//  Copyright © 2018年 zt. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
